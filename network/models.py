9a859e589b853124265c19147b6950b2e9cf4a03
Cody Sensenig <sinocanam@gmail.com>
2021-04-13 17:46:26 -0400

    Added models and updated index HTML

--- network/models.py
+++ network/models.py
@@ -1,6 +1,28 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.fields import CharField, DateTimeField, IntegerField
 
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    content = models.CharField(max_length=150)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author} said: {self.content} @ {self.created}'.format(self=self)
+
+class UserUser(models.Model):
+    following = models.ManyToManyField(User, related_name="followers")
+
+    def __str__(self):
+        return f'{self.following}'.format(self=self)
+
+class UserPost(models.Model):
+    user = models.ManyToManyField(User, related_name="user")
+    post = models.ManyToManyField(Post, related_name="post")
+
+    def __str__(self):
+        return f'{self.user} posted {self.post}'.format(self=self)
--- network/views.py
+++ network/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import Post, User
 
 
 def index(request):
@@ -61,3 +61,17 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+def post(request):
+    # 
+    if request.method == "POST":
+
+        content = request.POST["content"]
+        author = User.objects.get(username=request.user)
+
+        Post(content=content, author=author).save()
+        return HttpResponseRedirect(reverse("index"))
+    #
+    else:
+
+        return index(request)
